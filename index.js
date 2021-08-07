4309467a02ecdde80cf0149886b1a5ce6d4b18b5
legr4ndk <895148378@qq.com>
2021-08-07 17:13:17 +0800

    clear file

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ let mainWindow = null
 app.on("ready", () => {
     mainWindow = new browserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     })
     mainWindow.loadFile("./index.html")
     mainWindow.on("close", () => {
